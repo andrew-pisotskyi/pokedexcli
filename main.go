7d31947c14f4ca15578e0e27c65111c7daedcd7b
Andrew Pisotskyi <andrew.pisotskyi@gmail.com>
2024-03-01 14:02:59 +0200

    feat: map mapb commands

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"time"
+
+	"github.com/andrew-pisotskyi/pokedexcli/internal/pokeapi"
+)
 
 func main() {
-	fmt.Println("Hello, pokemon")
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
 }
